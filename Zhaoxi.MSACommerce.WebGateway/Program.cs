5218670571e5977c928d22d1c454bc11e8f1e635
刘泽西 <liu605430641@gmail.com>
2026-03-18 21:27:36 +0800

    增加网关swagger 访问 短信验证码swagger

--- Zhaoxi.MSACommerce.WebGateway/Program.cs
+++ Zhaoxi.MSACommerce.WebGateway/Program.cs
@@ -33,9 +33,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(options =>
                      {
-                         options.SwaggerEndpoint("/swagger/v1/swagger.json","Gateway V1");
-                         options.SwaggerEndpoint("/auth/swagger.json",      "AuthServer V1");
-                         options.SwaggerEndpoint("/user/swagger.json",      "UserService V1");
+                         options.SwaggerEndpoint("/swagger/v1/swagger.json",  "Gateway V1");
+                         options.SwaggerEndpoint("/auth/swagger.json",        "AuthServer V1");
+                         options.SwaggerEndpoint("/user/swagger.json",        "UserService V1");
+                         options.SwaggerEndpoint("/verification/swagger.json","验证码服务器 V1");
                      });
 }
 
