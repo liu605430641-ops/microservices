1c857e60529e7308671e0711dbb5d5cacb4679f0
刘泽西 <liu605430641@gmail.com>
2026-03-19 15:16:31 +0800

    添加core

---
+++ services/category/src/Zhaoxi.MSACommerce.CategoryService.Core/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using Zhaoxi.MSACommerce.SharedKernel.Domain;
+
+namespace Zhaoxi.MSACommerce.CategoryService.Core.Entities;
+
+public class Category : BaseAuditEntity, IAggregateRoot
+{
+    public string Name { get; set; } = null!;
+    public long ParentId { get; set; }
+    public bool IsParent { get; set; }
+    public int Sort { get; set; }
+    public ICollection<CategoryBrands> Brands { get; set; }
+}
---
+++ services/category/src/Zhaoxi.MSACommerce.CategoryService.Core/Entities/CategoryBrands.cs
@@ -0,0 +1,11 @@
+﻿using Zhaoxi.MSACommerce.SharedKernel.Domain;
+
+namespace Zhaoxi.MSACommerce.CategoryService.Core.Entities;
+
+public class CategoryBrands : BaseAuditEntity
+{
+    public long CategoryId { get; set; }
+    public long BrandId { get; set; }
+    
+    public List<Category> Categories { get; set; }
+}
