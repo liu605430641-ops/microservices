e69fb7ffce9d04480ea1e8fa130b274c3d1148ca
刘泽西 <liu605430641@gmail.com>
2026-03-20 11:27:31 +0800

    增加无删除uysingyi引用

--- Zhaoxi.MSACommerce.WebGateway/Program.cs
+++ Zhaoxi.MSACommerce.WebGateway/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Consul;
