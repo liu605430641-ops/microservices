4b72e8fd6547c6ddeef333609a9e9412268da56d
刘泽西 <liu605430641@gmail.com>
2026-03-29 18:26:00 +0800

    集成Apollo配置管理，新增ConfigurationBuilderExtensions类和项目文件，更新启动流程文档
    Signed-off-by: 刘泽西 <liu605430641@gmail.com>

---
+++ pakages/Zhaoxi.MSACommerce.Configuration/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Com.Ctrip.Framework.Apollo;
+using Com.Ctrip.Framework.Apollo.Enums;
+using Microsoft.Extensions.Configuration;
+using Winton.Extensions.Configuration.Consul;
+
+namespace Zhaoxi.MSACommerce.Configuration;
+
+public static class ConfigurationBuilderExtensions
+{
+    public static IApolloConfigurationBuilder AddConfigCenter(this ConfigurationManager configurationManager, string defaultNamespace)
+    {
+        configurationManager.AddConsul(
+            "zhaoxi-commerce/appsettings.json",
+            source =>
+            {
+                source.ReloadOnChange = true;
+            });
+
+        return configurationManager
+            .AddApollo(configurationManager.GetSection(nameof(ApolloOptions)))
+            .AddDefault(ConfigFileFormat.Json)
+            .AddNamespace(defaultNamespace, ConfigFileFormat.Json);
+    }
+}
